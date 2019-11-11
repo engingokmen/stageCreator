b1380dad6f1c93544f9280965fdf5d7431e93bb4
Engin Gokmen <engin.gokmen@icloud.com>
2019-11-11 20:32:32 +0300

    server

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port, () => console.log(`musicPlayer app listening on port ${port}!`)
+app.listen(port, () => console.log(`musicPlayer app listening on port ${port}!`));
